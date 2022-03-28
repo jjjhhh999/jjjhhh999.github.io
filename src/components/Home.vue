9764755b1168e166ccdf1bbb2a9298090fb0648e
jjjhhh999 <jaehoon.gu@gmail.com>
2022-03-28 15:46:54 +0900

    test

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -17,7 +17,7 @@
           <span
             class="home-title"
             :class="{ pgray: !nightMode, 'text-light': nightMode }"
-            >hello there!</span
+            >hello there!!!!!</span
           >
           <div>
             <p v-html="description"></p>
